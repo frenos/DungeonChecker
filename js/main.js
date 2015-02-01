5ea86beb199dd4789f872954d66f7b8c97160b9b
frenos <frenos@users.noreply.github.com>
2015-02-01 18:10:05 +0100

    added persitence
    Save/Load State in Localstorage
    add Button to clear Localstorage and reload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
 /**
  * Created by frenos on 01.02.2015.
  */
-var dungeondata = null
-$.getJSON("./data/dungeon_data_de.json", function(data){
-        dungeondata = data;
+
+function buildDungeonTable() {
+    $.getJSON("./data/dungeon_data_de.json", function (dungeondata) {
         dungeonscounter = 0;
-        $.each(dungeondata.dungeons, function(index, value){
-            if(index % 3 == 0)
+        $.each(dungeondata.dungeons, function (index, value) {
+            if (index % 3 == 0)
                 $("#dungeontable").append('<div class="row"></div>');
 
             var dungeonContentHtml = "";
-            dungeonContentHtml += '<div class="col-md-4"> <div class="thumbnail"> <img src="'+value.image+'">';
-            dungeonContentHtml += '<div class="caption"> <h3>'+value.title+'</h3>';
-            $.each(value.paths, function(pathindex, content){
-                dungeonContentHtml += '<label><input type="checkbox">'+content.name+'</label>'
+            dungeonContentHtml += '<div class="col-md-4"> <div class="thumbnail"> <img src="' + value.image + '">';
+            dungeonContentHtml += '<div class="caption"> <h3>' + value.title + '</h3>';
+            $.each(value.paths, function (pathindex, content) {
+                dungeonContentHtml += '<label><input type="checkbox" id="' + value.title + content.name + '">' + content.name + '</label>'
             });
             dungeonContentHtml += '</div></div></div>';
             $(".row:last").append(dungeonContentHtml);
+            });
+        addChangeToCheckbox();
+        loadCheckboxState();
         });
+}
+
+function addChangeToCheckbox() {
+    $("input:checkbox").change(function () {
+        localStorage[$(this).attr('id')] = this.checked;
     });
+}
 
+function loadCheckboxState() {
+    $("input:checkbox").each(function () {
+        var state = localStorage[$(this).attr('id')];
+        if (state == undefined)
+            state = false;
+        $(this).prop('checked', $.parseJSON(state));
+    })
+}
 
+function clearAllData() {
+    localStorage.clear();
+    location.reload();
+}
+
+$(document).ready(function () {
+    buildDungeonTable();
+});
