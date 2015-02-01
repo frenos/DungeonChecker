e3812790b0c64ea1fda37a2b428887dacef66b6a
frenos <frenos@users.noreply.github.com>
2015-02-01 20:40:48 +0100

    dailyreset and layout
    read/write timestamp of last save to reset if daily have reset already.
    changed layout to have headings overlay image and center controls
    fixed alert always showing

--- js/main.js
+++ js/main.js
@@ -11,13 +11,18 @@ function buildDungeonTable() {
 
             var dungeonContentHtml = "";
             dungeonContentHtml += '<div class="col-md-4"> <div class="thumbnail"> <img src="' + value.image + '">';
-            dungeonContentHtml += '<div class="caption"> <h3>' + value.title + '</h3>';
+            dungeonContentHtml += '<div class="caption post-image"> <h3>' + value.title + '</h3>';
+            dungeonContentHtml += '<div class="checkbox">';
             $.each(value.paths, function (pathindex, content) {
-                dungeonContentHtml += '<label><input type="checkbox" id="' + value.title + content.name + '">' + content.name + '</label>'
+                dungeonContentHtml += '<label class="checkbox-inline"><input type="checkbox" id="' + value.title + content.name + '">' + content.name + '</label>'
             });
-            dungeonContentHtml += '</div></div></div>';
+            dungeonContentHtml += '</div></div></div></div>';
             $(".row:last").append(dungeonContentHtml);
             });
+        $("div.thumbnail").each(function () {
+            $(this).children("div.caption").css(
+                "width", $(this).width() - 20 + "px");
+        });
         addChangeToCheckbox();
         loadCheckboxState();
         });
@@ -26,10 +31,31 @@ function buildDungeonTable() {
 function addChangeToCheckbox() {
     $("input:checkbox").change(function () {
         localStorage[$(this).attr('id')] = this.checked;
+        localStorage['lastSave'] = new Date();
     });
 }
 
 function loadCheckboxState() {
+    if (localStorage['lastSave'] != undefined)
+        var saveDate = new Date(localStorage['lastSave']);
+    var ResetDate = new Date();
+    ResetDate.setHours(1);
+    ResetDate.setMinutes(0);
+    if (saveDate < ResetDate) {
+        localStorage.clear();
+        $("#alert-container").html('<div class="alert alert-info alert-dismissable in">' +
+        '<button type="button" class="close" ' +
+        'data-dismiss="alert" aria-hidden="true">' +
+        '&times;' +
+        '</button>' +
+        'Daily Reset, all cleared!' +
+        '</div>');
+        window.setTimeout(function () {
+            $(".alert").fadeTo(1500, 0).slideUp(500, function () {
+                $(this).remove();
+            });
+        }, 1800);
+    }
     $("input:checkbox").each(function () {
         var state = localStorage[$(this).attr('id')];
         if (state == undefined)
